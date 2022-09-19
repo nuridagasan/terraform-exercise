bb1ee1ade51e01e08239f77e7e0b68c9724067a6
nuridagasan <dagasannuri@gmail.com>
2022-09-19 17:00:26 +0100

    added env. variables and output

--- main.tf
+++ main.tf
@@ -4,19 +4,19 @@ provider "aws" {
 }
 
 # Create a EC2 instance
-resource "aws_instance" "example" {
+resource "aws_instance" "web_server" {
   ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
 
   # Give a reference to security group
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  vpc_security_group_ids = [aws_security_group.web_server.id]
 
   # Exucute shell script for first boot
   # EOF, hdc syntax, allows to create multiline strings
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
   # If the user_data parameter changes, terraform terminates the
@@ -32,14 +32,27 @@ resource "aws_instance" "example" {
 
 # Create a security group to allow inbound TCP traffic on port 8080
 # outbound any IP
-resource "aws_security_group" "instance" {
-  name = "ubuntu-web-server-sg"
+resource "aws_security_group" "web_server" {
+  name = "web-server-sg"
 
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = var.server_port
+    to_port = var.server_port
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   
+}
+
+# Create a variable for DRY principle
+variable "server_port" {
+  description = "The server port will use for HTTP requests"
+  type = number
+  default = 8080
+}
+
+# Get public IP of web server instance
+output "public_ip" {
+  value = aws_instance.web_server.public_ip
+  description = "The public IP address of the web server"
 }
